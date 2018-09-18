2c55454a8a31c6474d09633e36888cbbbb992fcc
FreshKay <mimkolaj.jon@gmail.com>
2018-09-18 15:38:46 +0200

    Add ItemsContext

---
+++ ShopSite/DAL/ItemsContext.cs
@@ -0,0 +1,17 @@
+﻿using ShopSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ShopSite.DAL
+{
+    public class ItemsContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<ItemPosition> ItemPositions { get; set; }
+    }
+}
