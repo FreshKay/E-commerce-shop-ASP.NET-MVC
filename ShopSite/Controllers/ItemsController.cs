63b227da7e16f2da4272c78e4e3c9647f4df9c60
FreshKay <mikolaj.jon@gmail.com>
2018-10-23 23:18:20 +0200

    Revert "Add search help"
    This reverts commit ee21a4e592405b7a31c1770c33f2c9a99612b594.

--- ShopSite/Controllers/ItemsController.cs
+++ ShopSite/Controllers/ItemsController.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.UI;
 
 namespace ShopSite.Controllers
 {
@@ -31,7 +30,7 @@ namespace ShopSite.Controllers
         {
             var item = db.Items.Find(id);
             return View(item);
-        }        
+        }
 
     }
 }
