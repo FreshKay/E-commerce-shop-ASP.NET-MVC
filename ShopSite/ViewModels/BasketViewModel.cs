b7138021ac3fc5fa3775d01374ce1ba8994c3e9e
FreshKay <mikolaj.jon@gmail.com>
2018-10-31 21:29:03 +0100

    Add cart view

---
+++ ShopSite/ViewModels/BasketViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ShopSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopSite.ViewModels
+{
+    public class BasketViewModel
+    {
+        public List<BucketStatus> BucketStatus { get; set; }
+        public decimal PriceSum { get; set; }
+    }
+}
