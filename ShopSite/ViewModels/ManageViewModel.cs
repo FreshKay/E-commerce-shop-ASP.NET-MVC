0c60a525da546c7aca12ace449956c2dabac493e
FreshKay <mikolaj.jon@gmail.com>
2018-11-04 21:42:51 +0100

    Add manage view

--- ShopSite/ViewModels/ManageViewModel.cs
+++ ShopSite/ViewModels/ManageViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ShopSite.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,27 @@ namespace ShopSite.ViewModels
 {
     public class ManageViewModel
     {
+        public ChangePasswordViewModel ChangePasswordViewModel { get; set; }
+        public ShopSite.Controllers.ManageController.ManageMessageId? Message { get; set; }
+        public UsersData UsersData { get; set; }
+    }
+
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
     }
 }
